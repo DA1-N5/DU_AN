0098445b406ebf5f5c0dd30ac161d2bc0079954b
Tran Van Nam <vannamhdvt@gmail.com>
2021-11-12 00:41:30 +0700

    thêm DAO và validate

--- global.php
+++ global.php
@@ -1,3 +1,15 @@
 <?php
 $website = "/TEST";
+$url_admin = $website . "/admin/";
+$url_images = $website . "/images/";
+
+$image_path = $_SERVER['DOCUMENT_ROOT'] . $url_images;
+
+function save_file($name, $address){
+    $image = $_FILES[$name];
+    $fileName = $image['name'];
+    $part = $address . $fileName;
+    move_uploaded_file($image['tmp_name'], $part);
+}
+
 ?>
