e5a28fc8aceb8bb35617d9d607c4dbb185f02abb
tienpk200101 <phungkhactien2001@gmail.com>
2021-11-19 23:04:33 +0700

    ko xác định
    ko bt

--- business/client/clientpage.php
+++ business/client/clientpage.php
@@ -11,4 +11,28 @@ function client_detail(){
         "comment" => $comment
     ]);
 }
-?>
+
+function client_login() {
+    extract($_REQUEST);
+    if(empty($email) || empty($mat_khau)) {
+        $_SESSION['error'] = 'Không được để trống.';
+        header("location: $website/log/loginform.php");
+        die;
+    }
+    $user = getSelect_one('khach_hang', 'email', $email);
+    
+    if (empty($user) || md5($mat_khau) != $user['mat_khau']) {
+        $_SESSION['error'] = 'Mật khẩu hoặc tài khoản không đúng.';
+        header("location: $website/log/loginform.php");
+        die;
+    }
+    $_SESSION['user'] = $user;
+    $_SESSION['success'] = "<script>alert('Đăng nhập thành công.');</script>";
+    if(isset($_SESSION['id_tour'])){
+        $id_tour = $_SESSION['id_tour'];
+        unset($_SESSION['id_tour']);
+        header("location: $website/tour-detail.php?id=$id_tour");
+        die;
+    }
+    header("Location: $website/");
+}
--- views/client/login.php
+++ views/client/login.php
@@ -1,3 +1,17 @@
+<<<<<<< Updated upstream
+=======
+<?php
+session_start();
+require_once "../global.php";
+if(isset($_SESSION['user'])){
+    header("Location: <? BASE_URL/");
+    die;
+}
+if(isset($_GET['id_tour'])){
+    $_SESSION['id_tour'] = $_GET['id_tour'];
+}
+?>
+>>>>>>> Stashed changes
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,7 +28,11 @@
     <div class="auth-wrapper">
         <div class="auth-background"></div>
         <div class="auth-container">
+<<<<<<< Updated upstream
             <form class="auth-form" action="<?= BASE_URL ?>/save-login" method="post">
+=======
+            <form class="auth-form" action="login.php" method="post">
+>>>>>>> Stashed changes
                 <div class="auth-form--title">
                     <h1>Đăng Nhập vào tài khoản</h1>
                 </div>
@@ -39,8 +57,13 @@
                         <input type="password" class="form-control" name="mat_khau" id="password" placeholder="******">
                     </div>
                     <div class="mb-3 d-flex justify-content-between align-items-center">
+<<<<<<< Updated upstream
                         <a href="<?= BASE_URL ?>/sign-up" id="hihi">Đăng Kí ngay</a>
                         <a href="<?= BASE_URL ?>/forget" id="hihi">Quên mật khẩu?</a>
+=======
+                        <a href="./sign-up-form.php" id="hihi">Đăng Kí ngay</a>
+                        <a href="./changePass/forgetps-form.php" id="hihi">Quên mật khẩu?</a>
+>>>>>>> Stashed changes
                     </div>
                     <button type="submit" class="btn btn-blue mb-3">Đăng nhập</button>
 
