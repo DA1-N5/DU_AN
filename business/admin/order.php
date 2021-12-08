cb91e43c7c0cd6c19a5fb061085bc25053af3f11
loveonex <vannamhdvt@gmail.com>
2021-12-08 23:09:27 +0700

    sửa lại

--- business/admin/order.php
+++ business/admin/order.php
@@ -24,52 +24,57 @@ function order_add(){
 
 function order_save_add(){
     extract($_REQUEST);
-if(
-    checkEmpty($id_tour) == false ||
-    checkEmpty($ngay_di) == false ||
-    checkEmpty($noi_di) == false ||
-    checkEmpty($gia) == false ||
-    checkEmpty($email) == false 
+    if(
+        checkEmpty($id_tour) == false ||
+        checkEmpty($ngay_di) == false ||
+        checkEmpty($noi_di) == false ||
+        checkEmpty($gia) == false ||
+        checkEmpty($email) == false 
 
-){
-    $_SESSION['error'] = "Vui lòng không để trống !";
-    header("Location: " . BASE_URL . "/admin/order/add?id=$id_tour");
-    die;
-}
+    ){
+        $_SESSION['error'] = "Vui lòng không để trống !";
+        header("Location: " . BASE_URL . "/admin/order/add?id=$id_tour");
+        die;
+    }
 
-if(empty($nguoi_lon)){
-    $nguoi_lon = 0;
-}
-if(empty($tre_em)){
-    $tre_em = 0;
-}
+    if(empty($nguoi_lon)){
+        $nguoi_lon = 0;
+    }
+    if(empty($tre_em)){
+        $tre_em = 0;
+    }
 
-$user = getSelect_one("khach_hang", 'email', $email);
-if(empty($user)){
-    $_SESSION['error'] = "Email không có trong dữ liệu !";
-    header("Location: " . BASE_URL . "/admin/order/add?id=$id_tour");
-    die;
-}
-if(!checkInt(intval($nguoi_lon))){
-    $_SESSION['error'] = "Số lượng người lớn không đúng định dạng !";
-    header("Location: " . BASE_URL . "/admin/order/add?id=$id_tour");
-    die;
-}
+    $user = getSelect_one("khach_hang", 'email', $email);
+    if(empty($user)){
+        $_SESSION['error'] = "Email không có trong dữ liệu !";
+        header("Location: " . BASE_URL . "/admin/order/add?id=$id_tour");
+        die;
+    }
+    if(intval($user['trang_thai']) == 2){
+        $_SESSION['error'] = "Tài khoản khách hàng bị vô hiệu hóa !";
+        header("Location: " . BASE_URL . "/admin/order/add?id=$id_tour");
+        die;
+    }
+    if(!checkInt(intval($nguoi_lon))){
+        $_SESSION['error'] = "Số lượng người lớn không đúng định dạng !";
+        header("Location: " . BASE_URL . "/admin/order/add?id=$id_tour");
+        die;
+    }
 
-if(!checkInt(intval($tre_em))){
-    $_SESSION['error'] = "Số lượng trẻ em không đúng định dạng !";
-    header("Location: " . BASE_URL . "/admin/order/add?id=$id_tour");
-    die;
-}
+    if(!checkInt(intval($tre_em))){
+        $_SESSION['error'] = "Số lượng trẻ em không đúng định dạng !";
+        header("Location: " . BASE_URL . "/admin/order/add?id=$id_tour");
+        die;
+    }
 
-if(!checkInt(intval($gia))){
-    $_SESSION['error'] = "Giá không đúng định dạng !";
-    header("Location: " . BASE_URL . "/admin/order/add?id=$id_tour");
-    die;
-}
-$ngay_them = date('Y-m-d');
-insert_donhang($id_tour, $user['id'], $nguoi_lon, $tre_em, $ngay_di, $noi_di, $gia, $lich_trinh, $ngay_them, $_SESSION['admin']['id']);
-header("Location: " . BASE_URL . "/admin/order/list");
+    if(!checkInt(intval($gia))){
+        $_SESSION['error'] = "Giá không đúng định dạng !";
+        header("Location: " . BASE_URL . "/admin/order/add?id=$id_tour");
+        die;
+    }
+    $ngay_them = date('Y-m-d');
+    insert_donhang($id_tour, $user['id'], $nguoi_lon, $tre_em, $ngay_di, $noi_di, $gia, $lich_trinh, $ngay_them, $_SESSION['admin']['id']);
+    header("Location: " . BASE_URL . "/admin/order/list");
 }
 
 function order_update(){
